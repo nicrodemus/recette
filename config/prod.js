4638db0ad390787dd72de9655e43ae7e94987301
Niccolò Boscolo <niccolo.boscolo88@icloud.com>
2021-01-11 21:44:06 +0100

    keys finished

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+	idClient: process.env.GOOGLE_CLIENT_ID, 
+	clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+	cookieKey: process.env.COOKIE_KEY
+};
